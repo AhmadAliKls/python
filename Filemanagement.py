ffd331f9cc4ef526fbbfec5a401e9728ffe23007
Ahmadai <muhammadahmadaliklasra1@gmail.com>
2026-01-02 11:08:51 +0500

    Add word replacement and search functionality
    Implemented functions to replace and search words in a text file.

---
+++ Filemanagement.py
@@ -0,0 +1,40 @@
+def replace_word(change,new):
+    name_to_change = str(change)
+    name_new = str(new)
+    with open("practice.txt" , "r") as f:
+        content = f.read()
+    content = content.replace(name_to_change , name_new)
+    
+    with open("practice.txt" , "w") as f:
+        f.write(content)
+        return content
+
+
+def search_word(name_word):
+    line_count = 0
+    data = True
+    word = str(name_word)
+    def search_word(name_word):
+        line_count = 0
+        word = name_word
+    with open("practice.txt", "r") as f:
+        content = f.read()
+        for content in f:
+            line_count += 1
+            if word in content:
+                print(f"'{word}' found at line number: {line_count}")
+                return line_count
+    print(f"'{word}' not found in file")
+    return None
+function_perform = input("Enter 's' to search a word or 'r' to replace a word: ")
+if function_perform == 'r':
+    change = input("Enter the word to be replaced: ")
+    new = input("Enter the new word: ")
+    new_data = replace_word(change , new)
+    print(new_data)
+elif function_perform == 's':
+    name_word = input("Enter the word to be searched: ")
+    line_num = search_word(name_word)
+    print(line_num)
+else:
+    print("Invalid input")
